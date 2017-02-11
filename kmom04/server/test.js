5b893920166a1c07958016a0f8ec9a1b6658f5da
Todai88 <joabaj88@gmail.com>
2017-02-11 14:31:49 +0000

    finished supporting.bash

--- kmom04/server/test.js
+++ kmom04/server/test.js
@@ -1,6 +1,6 @@
 var util  = require('util'),
-    spawn = require('child_process').spawn,
-    supporting = spawn('. ./supporting.bash', (error, stdout, stderr) => {
+    spawn = require('child_process').exec,
+    supporting = exec('. ./supporting.bash', (error, stdout, stderr) => {
 
         if(error){
             console.error(`exec error: ${error}`);
