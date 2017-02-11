9c770b61732f54a43882bd46048edc108c906991
Todai88 <joabaj88@gmail.com>
2017-02-11 13:25:35 +0000

    changes

--- kmom04/server/supporting.bash
+++ kmom04/server/supporting.bash
@@ -11,6 +11,6 @@ else
 
 	echo "LINUX_PORT=1337" >> /etc/environment
 	export LINUX_PORT
-	echo "LINUX_PORT in 'env' " cat env | grep LINUX_PORT
-	echo "LINUX_PORT in '/etc/environment'" cat /etc/environment | grep LINUX_PORT
+	echo "LINUX_PORT in 'env': " $(cat env | grep LINUX_PORT)
+	echo "LINUX_PORT in '/etc/environment': " $(cat /etc/environment | grep LINUX_PORT)
 fi;
