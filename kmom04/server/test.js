838fc35fbbc02d8b6ed2d1bd9e5265910557f32b
Todai88 <joabaj88@gmail.com>
2017-02-11 14:23:34 +0000

    finished supporting.bash

--- kmom04/server/test.js
+++ kmom04/server/test.js
@@ -4,4 +4,10 @@ var util  = require('util'),
 
 bs.stdout.on('data', function (data) {    // register one or more handlers
   console.log('stdout: ' + data);
+
+  var child = spawn('bash', ['echo $LINUX_PORT']);
+
+  child.stdout.on('data', function(out)) {
+      console.log('Child stdout: ' + out);
+  }
 });
