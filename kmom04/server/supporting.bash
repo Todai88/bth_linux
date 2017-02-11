16813cca1070f4fbfb0ffe9412d8bc2e4c3833b5
Todai88 <joabaj88@gmail.com>
2017-02-11 13:35:48 +0000

    work

--- kmom04/server/supporting.bash
+++ kmom04/server/supporting.bash
@@ -8,9 +8,8 @@ else
 
 	echo "LINUX PORT NOT SET!"
 	echo "Setting port now"
-
-	echo "LINUX_PORT=1337" >> /etc/environment
+    LINUX_PORT=1337
 	export LINUX_PORT
-	echo "LINUX_PORT in 'env': " $(cat env | grep LINUX_PORT)
-	echo "LINUX_PORT in '/etc/environment': " $(cat /etc/environment | grep LINUX_PORT)
+	set LINUX_PORT
+	printf "LINUX_PORT in 'env': %s" "${!LINUX_PORT}"
 fi;
