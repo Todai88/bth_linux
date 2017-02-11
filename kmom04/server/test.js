83ee084ae78436dd60f1afba71a490e3021455b7
Todai88 <joabaj88@gmail.com>
2017-02-11 14:10:45 +0000

    finished supporting.bash

--- kmom04/server/test.js
+++ kmom04/server/test.js
@@ -1,14 +1,7 @@
 var util  = require('util'),
     spawn = require('child_process').spawn,
     bs    = spawn('bash', ['supporting.bash']);
-    un	  = spawn('bash', ['unset LINUX_PORT']);
 
 bs.stdout.on('data', function (data) {    // register one or more handlers
   console.log('stdout: ' + data);
-});
-
-un.stdout.on('data', function(data) {
-
-console.log('stdout: ' + data);
-
 });
