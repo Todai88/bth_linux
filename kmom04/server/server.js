b6a7cb75315919d0c1c99cd0a360bec22ebbe9f3
Joakim Kakaei <joabaj88@gmail.com>
2017-02-15 16:38:32 +0000

    all of it

--- kmom04/server/server.js
+++ kmom04/server/server.js
@@ -4,15 +4,18 @@
 "use strict";
 
 // Require the module
-var http = require("http");
-
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
 // Use the createServer function to create the simple server
 var server = http.createServer((req, res) => {
 
-    var ipAddress;
+    var ipAddress,
+        route;
 
     // Log incoming requests
     ipAddress = req.connection.remoteAddress;
+    route = url.parse(req.url).pathname;
     console.log("Incoming request from ip " + ipAddress);
 
     // Write header with text/plain as content type and 200 HTTP status code
@@ -20,6 +23,40 @@ var server = http.createServer((req, res) => {
 
     // Reply with a string
     res.end("Hello World\n");
+
+    // Switch (route) on the path.
+   switch (route) {
+       case "/":
+           // Home page route.
+           res.writeHead(200, { "Content-Type": "text/plain" });
+           res.end("Hello World!\n");
+       break;
+
+       case "/index.html":
+           // About page route.
+           var filename = path.join(__dirname, 'index.html'), data;
+           fs.readFile(filename, "utf8", (err, data){
+               if (err) {
+                   res.writeHead(500, { "Content-Type": "text/plain"});
+                   res.end("Internal server error");
+                   console.log("Internal server error");
+                   throw err;
+
+               }
+
+               res.writeHead(200, { "Content-Type": "text/html" });
+               res.end(data);
+
+                });
+       break;
+
+       default:
+           // Not found route.
+           res.writeHead(404, { "Content-Type": "text/plain" });
+           res.end("404. No route matching.\n");
+       break;
+   }
+
 });
 
 // Export the server as a module.
