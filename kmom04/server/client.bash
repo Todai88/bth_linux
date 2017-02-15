49a707a13f73a16fdfd0565c057c09b6f8a49dda
Todai88 <joabaj88@gmail.com>
2017-02-15 21:24:28 +0000

    test

--- kmom04/server/client.bash
+++ kmom04/server/client.bash
@@ -1,5 +1,44 @@
 #!/bin/bash
 
+
+# Script name
+SCRIPT=$( basename "$0" )
+
+# Script version
+VERSION="1.0.0a"
+
+
+
+#
+# Usage / help message.
+#
+
+function usage_help ()
+{
+	local txt=(
+	"Use this script to:"
+	""
+	"1.		Help you reverse strings."
+	"2.		Get the prime factors of numbers."
+	"3.		Watch ASCII StarWars (ep IV)."
+	""
+	"USAGE: $SCRIPT [options] <command> [arguments]"
+	""
+	"OPTIONS:"
+	" --help, 	 -h			Print help."
+	" --version, 	 -v			Print version."
+	""
+	"COMMAND: arguments in paranthesis ()"
+	" reverse(string s)		Reverses and prints string s"
+	" factors(args)			Prints the primefactors of submitted arguments."
+	" starwars()			Shows you an ASCII version of starwars over telnet."
+	)
+
+	printf "%s\n" "${txt[@]}"
+}
+
+function app-check_values()
+{
 if [[ $LINUX_PORT ]]; then
 
 	printf "LINUX_PORT in 'env': %s\n" "${LINUX_PORT}"
@@ -22,9 +61,58 @@ if [[ $LINUX_SERVER ]]; then
 else
 
 	echo "LINUX_SERVER NOT SET!"
-	echo "Defaulting to 178.62.110.17"
-    LINUX_SERVER=178.62.110.17
+	echo "Defaulting to localhost"
+    LINUX_SERVER=localhost
 	export LINUX_SERVER
 	set LINUX_SERVER
 	printf "LINUX_PORT in 'env': %s\n" "${LINUX_SERVER}"
 fi;
+}
+
+
+
+
+
+#
+# Checking and setting values.
+#
+app-check_values()
+
+#
+# Main script-body, iterates through submitted parameters
+#
+
+while (( $# ))
+do
+	case "$1" in
+
+		--help | -h)
+			usage_help
+			exit 0
+		;;
+
+		--version | -v)
+			version
+			exit 0
+		;;
+
+		hello 	\
+		|	html \
+		|	status \
+		|	sum		\
+		|	filter)
+			command=$1
+			shift
+			app-"$command" "$@"
+			exit 0
+		;;
+
+		*)
+			incorrect_usage "Options/command not recognized."
+			exit 1
+		;;
+	esac
+done
+
+incorrect_usage
+exit 1
