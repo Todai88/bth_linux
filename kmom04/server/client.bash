677355574a1476c09fb43f756b016fc5b1e6b0f7
Todai88 <joabaj88@gmail.com>
2017-02-15 22:15:02 +0000

    test

--- kmom04/server/client.bash
+++ kmom04/server/client.bash
@@ -130,12 +130,13 @@ function app-sum()
 	final_out="?"
 	while [ "$1" != "" ];
 		do
-		if [ "$2" == "" ]; then
-			final_out+="$1"
-		else
-			final_out+="$1&"
-		fi
-	done
+			if [ "$2" == "" ]; then
+				final_out+="$1"
+			else
+				final_out+="$1&"
+			fi
+			shift
+		done
 	echo $final_out
 	curl "${LINUX_SERVER}:${LINUX_PORT}/sum $@"
 }
