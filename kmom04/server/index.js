cb450d132c9723b185a9f8300ead353dfc8241d3
Joakim Kakaei <joabaj88@gmail.com>
2017-02-15 16:16:18 +0000

    all of it

--- kmom04/server/index.js
+++ kmom04/server/index.js
@@ -9,9 +9,15 @@ const fs = require("fs");
 const server = require("./server.js");
 const process = require("process");
 
-console.log("Found port environment variable: " + process.env.LINUX_PORT);
-// Start the server to listen on a port
-server.listen(process.env.LINUX_PORT);
+// Check if LINUX_PORT env is defined, else use predefined port 1337.
+if(process.env.LINUX_PORT !== undefined){
+    console.log("Found port environment variable: " + process.env.LINUX_PORT);
+    server.listen(process.env.LINUX_PORT);
+} else {
+    console.log("Couldn't find LINUX_PORT environment variable. Falling back to 1337." +
+                "Not like it makes any difference..");
+    server.listen(1337);
+}
 
 // Write pid to file
 var pidFile = path.join(__dirname, "pid");
