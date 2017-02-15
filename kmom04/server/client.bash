5876fec8e33624f3d71682d7e70321a20a8f5901
Todai88 <joabaj88@gmail.com>
2017-02-15 23:10:24 +0000

    finished server

--- kmom04/server/client.bash
+++ kmom04/server/client.bash
@@ -190,7 +190,7 @@ function app-all()
 {
 	final_out="1 2 3 4 5 10 20 30 40 41 42 43 44 45 50 60 70 80 90"
 	printf "Filter - using %s" "${final_out}"
-	app-filter "$final_out"
+	app-filter "${final_out}"
 }
 
 #
