f0ee4a65f73b975584a45c3d657989f39dbdf74b
Joakim Kakaei <joabaj88@gmail.com>
2017-02-17 11:00:07 +0000

    added xxx

--- own-stuff/fibonacci.py
+++ own-stuff/fibonacci.py
@@ -1,15 +1,17 @@
 def calc(prev, curr):
+  if (prev == 0 and curr == 0):
+    return 1
   return prev + curr
 
 def main_loop():
   previous = 0
-  current = 1
+  current = 0
 
-  for iterator in range(1,100):
+  for iterator in range(1,101):
     tmp = previous
     previous = current
     current = calc(tmp, current)
-    print(str(iterator) + "(" + str(tmp) + ", " + str(previous) + ")" + ": " + str(current))
+    print(str(iterator) + "(" + str(tmp) + ", " + str(previous) + ")" + ": " + str(tmp + previous))
   out = previous + current
   print(out)
 
