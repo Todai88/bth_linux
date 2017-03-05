1ff93469f4b56983fb91a08fc5e9658074226029
Todai88 <joabaj88@gmail.com>
2017-03-05 12:46:33 +0000

    Pushing for test

--- kmom06/gomoku/index-client.js
+++ kmom06/gomoku/index-client.js
@@ -195,6 +195,7 @@ rl.on("line", function(line) {
                 gomoku.place(x, y)
                 .then(value => {
                     console.log(value);
+                    console.log(value.boardIsWon);
                     rl.prompt();
                 })
                 .catch(err => {
