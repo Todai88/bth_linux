0f3cf88537b77ebd135425de1ce23bea430abe5a
Joakim Kakaei <joabaj88@gmail.com>
2017-03-20 14:53:52 +0100

    Pushing for the server

--- kmom10/bthappen/server/bthappen_server.js
+++ kmom10/bthappen/server/bthappen_server.js
@@ -82,7 +82,7 @@ router.get("/room/list", (req, res) => {
 
 router.get("/room/view/id/:number", (req, res) => {
 
-    var room = number;
+    var room = req.params.number;
     var message = "Attempting to get a list of all rooms.";
     try {
         server_body.getRoom(room);
