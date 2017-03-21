9cf0edda5b1c07dd65ba5837316d280509cf68d5
Todai88 <joabaj88@gmail.com>
2017-03-21 18:18:57 +0100

    pushing for project

--- kmom10/bthappen/server/server_logic.js
+++ kmom10/bthappen/server/server_logic.js
@@ -159,15 +159,15 @@ class server_logic {
         out_list = out_list.sort(function(a, b) {
             return b[1] - a[1];
         });
-        console.log(out_list);
-        console.log("Highest: " + out_list[0]);
+        return [object, out_list];
     }
     getFromSearch_prio(query) {
         var list = [];
         for (var object of this.list){
             //list.push(getPriority(object, query));
-            this.getPriority(object, query);
+            list.push(this.getPriority(object, query));
         }
+        return list;
     }
 
     getSize() {
