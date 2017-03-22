20ea5f208f34231bd5642947f3e7e0ff110daf4e
Joakim Kakaei <joabaj88@gmail.com>
2017-03-22 14:09:44 +0100

    Working on project

--- kmom10/bthappen/server/bthappen_server.js
+++ kmom10/bthappen/server/bthappen_server.js
@@ -32,7 +32,7 @@ function sendJSONResponse(res, content, code = 200) {
     res.writeHead(code, "Content-Type: application/json; charset=utf8");
     res.write(JSON.stringify(content, null, "    "));
     if(VERBOSE){
-        console.log(content.result.sal.length);
+        console.log(content.result);
         // if (content.result.length === 1) {
         //     console.log(content.sal);
         // } else console.log(content);
